b6e26f7624439332e21619d2e3e85f3818ed8b71
ArianOPGGP <aojeda_24@alu.uabcs.mx>
2026-06-03 22:04:57 -0700

    Home, Marca, Modelo, Categoria , Listas

--- src/Controllers/HomeController.java
+++ src/Controllers/HomeController.java
@@ -1,11 +1,16 @@
 package Controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import Models.HomeModel;
 import Views.AuthView;
 import Views.HomeView;
 
 public class HomeController {
 	private HomeView hv;
 	private AuthView av;
+	private HomeModel hm;
 	
 	public HomeController(){
 		hv = new HomeView();
@@ -15,4 +20,34 @@ public class HomeController {
 		hv.showHome();
 	}
 	
+	//REGRESA LISTA DE ESTADOS
+	public ArrayList<String> getListaEstados() {
+		return new ArrayList<>(Arrays.asList("Rentado", "Disponible", "Mantenimiento", "Inactivo"));
+	}
+	
+	//REGRESA LISTA DE PRECIOS
+	public ArrayList<Double> getListaPrecios() {
+		return new ArrayList<>(Arrays.asList(75.0, 100.0, 150.0, 175.0, 200.0, 250.0, 275.0, 300.0, 350.0, 375.0, 400.0));
+    }
+	
+	//REGRESA LISTA DE AÑOS 
+	public ArrayList<Integer> getListaAnios() {
+		return new ArrayList<>(Arrays.asList(2028, 2027, 2026, 2025, 2024, 2023, 2022, 2021, 2020));
+    }
+	
+	//REGRESA LISTA DE MARCAS
+	 public ArrayList<String> getListaMarcas() {
+		 return hm.getListaMarcas();
+	 }
+	 
+	 //REGRESA LISTA DE MODELOS DEPENDIENDO DE LA MARCA
+	 public ArrayList<String> getListaModelos(String nombreMarca) {
+		 return hm.getListaModelos(nombreMarca);
+	 }
+	
+	 //REGRESA LA CATEGORIA DEL VEHICULO CON SU ID
+	 public String getCategoriaVehiculo(int id_vehiculo) {
+		 return hm.getCategoriaVehiculo(id_vehiculo);
+	 }
+	 
 }
